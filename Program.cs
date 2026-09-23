0678f50f431cca79cc8e19566e51d2b6a5da393f
bitsozs <yuwmuks@gmail.com>
2026-09-23 14:03:02 +0700

    Adding checkpoint

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int totalSteps = 30;
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"Loading... Step {i} of {totalSteps}");
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Checkpoint reached!");
+    }
+}
+
+Console.WriteLine("\nDungeon is ready. Enter now");
